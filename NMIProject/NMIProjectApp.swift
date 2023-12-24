d771096b0239b46a88aac37a6d8d1739bb7458f7
GhadahAlb <ghadah.b13@gmail.com>
2023-12-24 10:59:16 +0300

    Initial Commit

---
+++ NMIProject/NMIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NMIProjectApp.swift
+//  NMIProject
+//
+//  Created by Ghadah Albassam on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct NMIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
